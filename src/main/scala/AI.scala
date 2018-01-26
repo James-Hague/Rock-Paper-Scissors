685cfb6baec0030321e02a173b29eab25f1339fe
James-Hague <33607309+James-Hague@users.noreply.github.com>
2018-01-26 14:43:19 +0000

    yet another commit

--- src/main/scala/AI.scala
+++ src/main/scala/AI.scala
@@ -1,7 +1,16 @@
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
+import Array._
 
-object AI {
+object AI  {
+
+  def testList = GameMain.dumbArrayB
+
+
+
   def Skynet: String = {
+
+    if (testList.isEmpty) {
       val aiRandom = new scala.util.Random().nextInt(3)
       aiRandom match {
         case 0 => "Rock".toLowerCase
@@ -9,9 +18,18 @@ object AI {
         case 2 => "Paper".toLowerCase
         case _ => "Error".toLowerCase
       }
+    } else {
+      val result = testList.groupBy(identity).maxBy(_._2.size)._1
+      result match {
+        case "rock" =>  "Paper"
+        case "paper" => "Scissors"
+        case "scissors" => "rock"
+        case _ => "Error!"
+      }
     }
 
-  
+  }
+
   def DumbAi:String = {
     val aiRandom = new scala.util.Random().nextInt(3)
     aiRandom match {
--- src/main/scala/GameMain.scala
+++ src/main/scala/GameMain.scala
@@ -1,39 +1,62 @@
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ListBuffer}
+import Array._
 
-object GameMain extends  App {
 
-  var dumbMoves = List()
 
-  def compareStrings(string1:String,string2:String) = {
-    (string1.toLowerCase, string2.toLowerCase) match {
-      case ("rock", "paper") => "Player 2 Wins"
-      case ("rock", "scissors") => "Player 1 Wins"
-      case ("rock", "rock") => "DRAW"
-      case ("paper", "scissors") => "Player 2 Wins"
-      case ("paper", "rock") => "Player 1 Wins"
-      case ("paper", "paper") => "DRAW"
-      case ("scissors", "rock") => "Player 2 Wins"
-      case ("scissors", "paper") => "Player 1 Wins"
-      case ("scissors", "scissors") => "DRAW"
-      case _ => "Unknown Entry"
+object GameMain extends App {
+  var dumbArrayB = ListBuffer[String]()
+
+  var dumbWin = 0
+  var skynetWin = 0
+  var draw = 0
+
+
+    def compareStrings(string1: String, string2: String) = {
+      (string1.toLowerCase, string2.toLowerCase) match {
+        case ("rock", "paper") => "/////////////Player 2 Wins\\\\\\\\\\"
+        case ("rock", "scissors") => "**********Player 1 Wins************"
+        case ("rock", "rock") => "DRAW"
+        case ("paper", "scissors") => "/////////////Player 2 Wins\\\\\\\\\\"
+        case ("paper", "rock") => "**********Player 1 Wins************"
+        case ("paper", "paper") => "DRAW"
+        case ("scissors", "rock") => "/////////////Player 2 Wins\\\\\\\\\\"
+        case ("scissors", "paper") => "**********Player 1 Wins************"
+        case ("scissors", "scissors") => "DRAW"
+        case _ => "Unknown Entry"
+      }
     }
-  }
 
-  def playerVsComputer(computer: String):String = {
-    val playerInput = scala.io.StdIn.readLine("Enter your Choice  ")
-    println(s"Player Choice: $playerInput Computer Choice: $computer")
-    compareStrings(playerInput.toLowerCase, computer.toLowerCase)
-  }
+    def playerVsComputer(computer: String): String = {
+      val playerInput = scala.io.StdIn.readLine("Enter your Choice  ")
+      println(s"Player Choice: $playerInput Computer Choice: $computer")
+      compareStrings(playerInput.toLowerCase, computer.toLowerCase)
+    }
 
-  def ComputerVsComputer ={
-    val p1 = AI.Skynet
-    val p2 = AI.DumbAi
-    println(s"Skynet Choice: $p1 Dumb AI Choice: $p2")
-    compareStrings(p1.toLowerCase, p2.toLowerCase)
-  }
+    def ComputerVsComputer = {
+      val p1 = AI.Skynet
+      val p2 = AI.DumbAi
 
-  for (i <- 1 to 10) {
-    println(ComputerVsComputer)
-  }
+      println(s"**********Skynet Choice***********: $p1 /////////Dumb AI Choice////////: $p2")
+      dumbArrayB += p2
+
+      val results = compareStrings(p1.toLowerCase, p2.toLowerCase)
+      println(results)
+      results match {
+        case "**********Player 1 Wins************" => skynetWin += 1
+        case "/////////////Player 2 Wins\\\\\\\\\\" => dumbWin += 1
+        case "DRAW" => draw += 1
+      }
+      println("SKYNET SCORE  " + skynetWin,"  DUMB AI SCORE "  + dumbWin,  "  DRAW " + draw)/////////////
+
+
+    }
+
+  for (i <- 1 to 1000) {
+      println(ComputerVsComputer)
+
+     // println("ARRAYTEST" + dumbArrayB)
+    }
+
+
 }
 
